b3b4e9a2e4a6e7b1bb9d7b9e1ee06f7db750b606
Jean Frg <jeanfrg@gmail.com>
2015-02-03 23:42:49 +0100

    betsson 1 css is ready.. now for the js!

---
+++ sauce/betsson_1/1/js/script.js
@@ -0,0 +1,46 @@
+(function($) {
+    var menu = [{
+        title: "Start",
+        href: "/"
+    }, {
+        title: "Sportsbook",
+        href: "/sportsbook",
+        items: [{
+            title: "Live",
+            href: "/live"
+        }, {
+            title: "Prematch",
+            href: "/prematch"
+        }]
+    }, {
+        title: "Casino",
+        href: "/casino",
+        items: [{
+            title: "Live casino",
+            href: "/live"
+        }, {
+            title: "Casino",
+            href: "/casino",
+            items: [{
+                title: "Table games",
+                href: "/table-games"
+            }, {
+                title: "Slots",
+                href: "/slots"
+            }, {
+                title: "Mini games",
+                href: "/mini-games"
+            }, {
+                title: "Video slots",
+                href: "/video-slots"
+            }]
+        }, {
+            title: "Games",
+            href: "/games"
+        }]
+    }, {
+        title: "Scratch",
+        href: "/scratch"
+    }];
+
+}(jQuery));
